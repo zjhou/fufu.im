64152634478b68c4f28d1216b54f041eb7168cb7
zhoujiahao <z@zjh.im>
2018-07-14 09:20:13 +0800

    refactor(BlogApi): 重构 queryApi，去除对 prismic-javascript 的依赖。

--- src/app/js/service/BlogApi.js
+++ src/app/js/service/BlogApi.js
@@ -1,4 +1,3 @@
-import Prismic from 'prismic-javascript';
 import localforage from 'localforage';
 import Config from '../../../config/blog.config';
 import {Blog, NavItem, Post} from './Dto';
@@ -29,20 +28,28 @@ const respFormatter = (resp, filter, singleResult) => {
     }
 };
 
-const queryAPI = function (queryFun, filter, singleResult) {
-    return Prismic
-        .getApi(Config.apiEndPoint, {})
-        .then(queryFun)
-        .then(function (response) {
-            return respFormatter(response, filter, singleResult);
-        }, function (err) {
-            return Promise.reject(err);
-        });
+const queryAPI = async function (option, filter, singleResult) {
+    const blogConfig = localforage.getItem('blogConfig');
+    const URL = `${Config.apiEndPoint}/documents/search?`
+        + `page=${option.pagenow}&pageSize=${option.pagesize || (blogConfig || Config).pagesize}`
+        + `&orderings=[${option.orderings}]`
+        + `&ref=${await getBlogVer()}&q=[[at(document.type, "${option.type}")]]`;
+
+    return Get(URL, 'json')
+        .then(response => respFormatter(response, filter, singleResult))
+        .catch(Promise.reject);
 };
 
 const getBlogConfig = async () => {
+    let cachedConfig = await localforage.getItem('blogConfig');
+    if(cachedConfig) return cachedConfig;
+
     let config = await queryAPI(
-        api => api.query(Prismic.Predicates.at('document.type', Config.docType.blog)),
+        {
+            pagesize: 1,
+            type: Config.docType.blog,
+            pagenow: 1
+        },
         Blog.translate,
         true
     );
@@ -50,13 +57,22 @@ const getBlogConfig = async () => {
     return config;
 };
 
-const getBlogVer = async () => JSON.parse(await Get(Config.apiEndPoint)).refs[0].ref;
+const getBlogVer = async () => {
+    let cachedVer = await localforage.getItem('blogVer');
+    if(cachedVer) return cachedVer;
+    let ver = JSON.parse(await Get(Config.apiEndPoint)).refs[0].ref;
+    await localforage.setItem('blogVer', ver);
+    return ver;
+};
 
 const getNavItems = async () => {
     let results = await queryAPI(
-        api => api.query(Prismic.Predicates.at('document.type', Config.docType.navItem), {
-            orderings: '[document.data.order]',
-        }),
+        {
+            type: Config.docType.navItem,
+            pagenow: 1,
+            pagesize: 6,
+            orderings: 'document.data.order'
+        },
         NavItem.translate,
     );
     let items = await resolvePromise(results.list);
@@ -83,14 +99,15 @@ const getPosts = async (type, pagenow, hasNextPage) => {
     let results = cachedPosts
         ? await respFormatter(cachedPosts, Post.translate)
         : await queryAPI(
-            api => api.query(Prismic.Predicates.at('document.type', type), {
-                pageSize:pagesize,
-                page: pagenow
-            }),
+            {
+                type: type,
+                pagesize: pagesize,
+                pagenow: pagenow
+            },
             Post.translate
         );
     if(hasNextPage){
-        await getPostsWithWorker(type, pagenow + 1, pagesize, cachedConfig.version);
+        await getPostsWithWorker(type, pagenow + 1, pagesize, await getBlogVer());
     }
     results.list = await resolvePromise(results.list);
     results.pagenow = pagenow;
