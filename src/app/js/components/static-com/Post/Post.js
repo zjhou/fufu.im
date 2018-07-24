9d203163c03ff07c637b72687e1deeb090c4cbda
zhoujiahao <z@zjh.im>
2018-07-24 17:31:45 +0800

    refactor(Post): 简化 Post 组件。

--- src/app/js/components/static-com/Post/Post.js
+++ src/app/js/components/static-com/Post/Post.js
@@ -1,47 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import config from '../../../../../config/blog.config';
 import './style.scss';
-
-class Content extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    shouldComponentUpdate(nextProps) {
-        return nextProps.content !== this.props.content
-            || nextProps.type !== this.props.type;
-    }
-
-    componentDidMount() {
-        this.updateContent();
-    }
-
-    componentDidUpdate() {
-        this.updateContent();
-    }
-
-    updateContent() {
-        this.cont.innerHTML = '';
-        let props = this.props;
-        switch (props.type) {
-        case config.docType.photo:
-            this.cont.append(props.content);
-            break;
-        default:
-            this.cont.innerHTML = props.content;
-        }
-    }
-
-    render() {
-        return <div className="markdown-body" ref={ref => this.cont = ref}/>;
-    }
-}
-
-Content.propTypes = {
-    type: PropTypes.string.isRequired,
-    content: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
-};
 
 const Post = (props) => (
     <article>
@@ -49,10 +8,7 @@ const Post = (props) => (
             <span className="title high-light">{props.title}</span>
             <time className="high-light">{props.date}</time>
         </header>
-        <Content
-            type={props.type}
-            content={props.content}
-        />
+        <div className="markdown-body" dangerouslySetInnerHTML={{__html: props.content}}/>
     </article>
 );
 
