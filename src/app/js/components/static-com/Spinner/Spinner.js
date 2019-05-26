94fbc7127379271f7e59a253e7d60b994de483f9
ZjHOU <z@zjh.im>
2019-05-26 09:59:41 +0800

    fix: update spinner

--- src/app/js/components/static-com/Spinner/Spinner.js
+++ src/app/js/components/static-com/Spinner/Spinner.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import './style.scss';
 const spinner = () => <div className="lds-ellipsis"><div/><div/><div/><div/></div>;
-export default spinner;
+const walkingSquare = () => {
+    return (
+        <div className="spinner">
+            <div className="square1" />
+            <div className="square2" />
+            <div className="square3" />
+        </div>
+    );
+};
+
+export default walkingSquare;
