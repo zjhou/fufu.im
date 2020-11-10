c7ee8f17fcabbe0ef40a17094333ec021a5cdae2
bbbottle <z@zjh.im>
2020-11-10 17:12:06 +0800

    fix: https://github.com/zjhou/fufu.im/issues/25

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>MOON's BLOG</title>
+        <title>Moon's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
